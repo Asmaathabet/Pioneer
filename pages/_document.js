904b3a20ef08ee3133cd3e5fafd97810a8c706c3
Asmaathabet <asmathabet2009@hotmail.com>
2020-12-04 16:08:51 +0200

    - add js bundle for bootstrap - create config file for next.

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,9 @@ class MyDocument extends Document {
         <Html lang="en">
             <Head>
                 <meta name="description" content ="pioneer Store for products built by Next JS" />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
-                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" ></script>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" />
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" />
                 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
             </Head>
             <body>
